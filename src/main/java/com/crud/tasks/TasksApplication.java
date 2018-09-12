48f06e36452262e8afcc35a46e76b8cbab5ef0a9
psadovski <pawelsadowski92@gmail.com>
2018-09-12 21:27:18 +0200

    task182

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
 public class TasksApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(TasksApplication.class, args);
     }
-
-   //@Override
-    //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        //return application.sources(TasksApplication.class);
-    //}
 }
