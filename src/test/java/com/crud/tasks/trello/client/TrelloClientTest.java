d25a2e87f348ddaf9ad30df5867800f3743b7ed3
psadovski <39880148+psadovski@users.noreply.github.com>
2018-09-21 10:38:28 +0200

    Task19.1 (#14)

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -47,7 +47,7 @@ public class TrelloClientTest {
         // Given
         TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
         trelloBoards[0] = new TrelloBoardDto("test_id", "test_board", new ArrayList<>());
-        URI uri = new URI("http://test.com/members/pawesadowski8/boards?key=test&token=test&fields=name,id&lists=all");
+        URI uri = new URI("http://test.com/members/null/boards?key=test&token=test&fields=name,id&lists=all");
 
         when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
 
