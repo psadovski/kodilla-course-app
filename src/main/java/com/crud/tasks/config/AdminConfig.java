3147d87b5e59240b409331202e53dc27d8380039
psadovski <39880148+psadovski@users.noreply.github.com>
2018-10-19 14:24:44 +0200

    Task24.2 (#27)

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,16 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${company.name}")
+    private String companyName;
+
+    @Value("{company.email}")
+    private String companyMail;
+
+    @Value("{company.phone}")
+    private String companyPhone;
 }
